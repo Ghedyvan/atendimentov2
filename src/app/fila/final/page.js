51a2287f70e9d48304faf5c73269c3f49a4268cf
Castelo <90470038+Pedr0Castel0@users.noreply.github.com>
2024-03-25 20:07:05 -0300

    ✨ Adiciona estado para armazenar dados de pacientes e busca de pacientes na montagem do componente

--- src/app/fila/final/page.js
+++ src/app/fila/final/page.js
@@ -1,10 +1,40 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { Card, CardBody, Input, Button } from "@nextui-org/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function UserPage() {
+  const [paciente, setPaciente] = useState([]); // Definindo o estado para armazenar os dados
+
+  const mount = async () => {
+    try {
+      const pacientes = await fetch(
+        "https://flask-production-75af.up.railway.app/paci",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const jsonData = await pacientes.json();
+      const cpf = localStorage.getItem("cpf");
+      jsonData.patients.map((paciente) => {
+        if (paciente.cpf === cpf) {
+          setPaciente(paciente);
+        }
+      });
+    } catch (error) {
+      toast.error("Erro ao buscar pacientes");
+    }
+  };
+
+  useEffect(() => {
+    mount();
+  }, []); // Chamada do método mount quando o componente é montado
+
   const router = useRouter();
   return (
     <div className="w-screen bg-gradient-to-r from-[#006FEE] to-blue-900 h-screen flex items-center justify-center">
@@ -13,10 +43,7 @@ export default function UserPage() {
           <Image src="/oxemed.png" alt="Logo" width={300} height={200} />
           <h1 className="mt-8 text-[26px] ">Você ingressou na fila!</h1>
           <h2 className="mt-4 text-[22px]">Sua posição:</h2>
-          <p className="text-[82px] text-bold">12</p>
-          <h2 className="mb-8 text-[26px]">
-            Posição atual da fila: <span className="font-bold">6</span>
-          </h2>
+          <p className="text-[82px] text-bold">{paciente.position}</p>
           <Button
             onClick={() => router.push("/fila")}
             size="lg"
